22dc0dbef9c7850568d19b757799dd2a6f4f0458
XenderBell <belovas2004@yandex.ru>
2025-06-28 20:00:27 +0300

    Update ConverterJSON.h

--- include/ConverterJSON.h
+++ include/ConverterJSON.h
@@ -1,1 +1,20 @@
-//gtgadt
+#pragma once
+#include <vector>
+#include "../include/index.h"
+
+class ConverterJSON {
+public:
+
+    ConverterJSON() = default;
+
+    std::vector <std::string> GetTextDocuments(std::string configWay);
+
+    int GetResponsesLimit(std::string configWay);
+
+    std::vector <std::string> GetRequests(std::string requestsWay);
+
+    void putAnswers(std::vector <std::vector<RelativeIndex>> answers, std::string answersWay, int max_responses);
+
+    void launch(std::string configWay);
+
+};
