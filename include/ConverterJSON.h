a12dad98a5c9fdfe73fbffa3042e324f1206fbd0
XenderBell <belovas2004@yandex.ru>
2025-07-06 15:15:06 +0300

    Update ConverterJSON.h

--- include/ConverterJSON.h
+++ include/ConverterJSON.h
@@ -1,20 +1,29 @@
 #pragma once
 #include <vector>
 #include "index.h"
+#include <string>
 
 class ConverterJSON {
+    std::string configWay;
+    std::string requestsWay;
+    std::string answersWay;
 public:
 
-    ConverterJSON() = default;
+    ConverterJSON(const std::string& config = "config.json",
+                  const std::string& requests = "requests.json",
+                  const std::string& answers = "./../../config/answers.json"):
+                  configWay(config),
+                  requestsWay(requests),
+                  answersWay(answers) {};
 
-    std::vector <std::string> GetTextDocuments(std::string configWay);
+    std::vector <std::string> GetTextDocuments();
 
-    int GetResponsesLimit(std::string configWay);
+    int GetResponsesLimit();
 
-    std::vector <std::string> GetRequests(std::string requestsWay);
+    std::vector <std::string> GetRequests();
 
-    void putAnswers(std::vector <std::vector<RelativeIndex>> answers, std::string answersWay, int max_responses);
+    void putAnswers(std::vector <std::vector<RelativeIndex>> answers, int max_responses);
 
-    void launch(std::string configWay);
+    void launch();
 
 };
