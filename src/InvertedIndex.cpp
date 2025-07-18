66976150358f5425e09e5b366bb9207a097b0259
XenderBell <belovas2004@yandex.ru>
2025-07-18 13:35:45 +0300

    Update InvertedIndex.cpp

--- src/InvertedIndex.cpp
+++ src/InvertedIndex.cpp
@@ -4,23 +4,24 @@
 #include <thread>
 #include <algorithm>
 
-void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs){
-    for (int i = 0; i < input_docs.size(); i++){
-        docs.push_back(input_docs[i]);
-    }
-    std::vector<std::thread> threads;
+std::vector<Entry> InvertedIndex::GetWordCount(const std::string& word){
+    std::vector<Entry> entries;
     for (int i = 0; i < docs.size(); i++){
-        threads.push_back(std::thread([&, i]() {
-            std::vector<std::string> words;
-            split_text(docs[i], std::ref(words));
-            for (int j = 0; j < words.size(); j++) {
-                if (!freq_dictionary.contains(words[j])) {
-                    freq_dictionary[words[j]] = GetWordCount(words[j]);
-                }
+        Entry newEntry;
+        newEntry.doc_id = i;
+        int wordEntries = 0;
+        std::vector<std::string> splited;
+        split_text(docs[i], splited);
+        for (int j = 0; j < splited.size(); j++){
+            if (splited[j] == word){
+                wordEntries += 1;
             }
-        }));
+        }
+        newEntry.count = wordEntries;
+        if (newEntry.count > 0)
+            entries.push_back(newEntry);
     }
-    std::for_each(threads.begin(), threads.end(), [](auto &it){it.join();});
+    return entries;
 }
 
 std::vector<Entry> InvertedIndex::GetWordCount(const std::string& word){
